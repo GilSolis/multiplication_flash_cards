c5f072423d82e95674b3e372f1e87066911a1ca3
Gil Solis <gilsolisuw@gmail.com>
2021-01-25 20:23:44 -0800

    added event listener

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+
 // generates random # 1-12
 // alert((Math.floor(Math.random() * (13 - 1) + 1)))
 //     (Math.floor(Math.random() * (13 - 1) + 1))
+
+document.getElementById('showAnswer').addEventListener('click', showAnswer)
+document.getElementById('next').addEventListener('click', showNext)
+// document.getElementById('again').addEventListener('click', searchAgain);
+
+function showAnswer() {
+    alert("Answer working")
+    console.log('working')
+}
+
+function showNext() {
+    alert('Show next working')
+}
